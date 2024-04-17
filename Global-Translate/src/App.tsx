e02e53578305b5fa59fe2dfd656d22db503fadc3
Jorge Esquiva <jorgeesquivait@gmail.com>
2024-04-18 01:39:48 +0200

    starting

---
+++ Global-Translate/src/App.tsx
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Globlal Translate</h1>
+    </div>
+  );
+};
+export default App;
